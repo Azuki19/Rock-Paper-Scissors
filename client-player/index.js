8ff19478a8d39e8bbe0185feba85b8439e5ee53b
Azuki19 <analucia27259@gmail.com>
2024-08-30 08:36:08 -0500

    main

--- client-player/index.js
+++ client-player/index.js
@@ -1,62 +1,96 @@
-document.getElementById('player-form').addEventListener('submit', createUser);
+	let socket = io("http://localhost:5050", { path: "/real-time" });
 
-document.querySelectorAll('.selectable-item').forEach((item) => {
-	item.addEventListener('click', function () {
-		this.previousElementSibling.checked = true;
-	});
-});
+	document.getElementById('player-form').addEventListener('submit', createUser);
 
-async function createUser(event) {
-	event.preventDefault();
-	renderLoadingState();
-
-	try {
-		const name = document.getElementById('name').value;
-		const item = document.querySelector('input[name="item"]:checked').value;
-
-		const player = {
-			name: name,
-			item: item,
-		};
-
-		const response = await fetch('http://localhost:5050/user', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(player),
+	document.querySelectorAll('.selectable-item').forEach((item) => {
+		item.addEventListener('click', function () {
+			this.previousElementSibling.checked = true;
 		});
+	});
 
-		if (!response.ok) {
-			throw new Error('Network response was not ok');
+	socket.emit("identify", "user");
+
+	socket.on('update-counter', (count) => {
+		const counterElement = document.getElementById('counter');
+		if (counterElement) {
+			counterElement.textContent = count;
 		}
+	});
 
-		renderData(player);
-	} catch (error) {
-		renderErrorState();
+	async function createUser(event) {
+		event.preventDefault();
+		renderLoadingState();
+
+		try {
+			const name = document.getElementById('name').value;
+			const item = document.querySelector('input[name="item"]:checked').value;
+
+			const player = {
+				name: name,
+				item: item,
+			};
+
+			const response = await fetch('http://localhost:5050/user', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(player),
+			});
+
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+
+			renderData(player);
+
+			// Emitir el evento "play" al servidor con el nombre y el item
+			socket.emit('play', player);
+
+			// Ocultar el formulario después de crear el usuario
+			document.getElementById('player-form').style.display = 'none';
+
+		} catch (error) {
+			renderErrorState();
+		}
 	}
-}
 
-function renderErrorState() {
-	const container = document.getElementById('data-container');
-	container.innerHTML = '<p>Failed to load data</p>';
-	console.log('Failed to load data');
-}
 
-function renderLoadingState() {
-	const container = document.getElementById('data-container');
-	container.innerHTML = '<p>Loading...</p>';
-	console.log('Loading...');
-}
+	function renderErrorState() {
+		const container = document.getElementById('data-container');
+		container.innerHTML = '<p>Failed to load data</p>';
+		console.log('Failed to load data');
+	}
 
-function renderData(data) {
-	console.log(data);
+	function renderLoadingState() {
+		const container = document.getElementById('data-container');
+		container.innerHTML = '<p>Loading...</p>';
+		console.log('Loading...');
+	}
 
-	const container = document.getElementById('data-container');
-	container.innerHTML = ''; // Limpiar datos anteriores
-	const section = document.createElement('section');
-	section.className = 'item';
-	section.innerHTML = `Player created!
-  ${data.name} chose ${data.item}`;
-	container.appendChild(section);
-}
+	function renderData(data) {
+		console.log(data);
+
+		const container = document.getElementById('data-container');
+		container.innerHTML = ''; // Limpiar datos anteriores
+		const section = document.createElement('section');
+		section.className = 'item';
+		section.innerHTML = `Player created! ${data.name} chose ${data.item}`;
+		container.appendChild(section);
+	}
+
+	// Al inicio, deshabilitar el formulario y mostrar el mensaje de espera
+	document.getElementById('player-form').style.display = 'none';
+	document.getElementById('waiting-message').style.display = 'block';
+
+	socket.on('start-countdown', () => {
+		// Habilitar el formulario y ocultar el mensaje de espera cuando comience el juego
+		document.getElementById('player-form').style.display = 'block';
+		document.getElementById('waiting-message').style.display = 'none';
+
+		// Mostrar el contador
+		document.getElementById('counter').style.display = 'block';
+
+
+	});
+
--- client-results-screen/index.js
+++ client-results-screen/index.js
@@ -1,92 +1,96 @@
+let socket = io("http://localhost:5050", { path: "/real-time" });
+
+socket.emit("identify", "result");
+
 let count = 0;
+let interval;
 
-const interval = setInterval(() => {
-	const sec = document.getElementById('seconds');
+document.getElementById('start-game').addEventListener('click', () => {
+  socket.emit('start-game');
+});
 
-	if (sec) {
-		sec.textContent = count;
-	}
+socket.on('start-countdown', () => {
+  clearInterval(interval);
+  count = 10;
 
-	count++;
+  interval = setInterval(() => {
+    const sec = document.getElementById('seconds');
+    if (sec) {
+      sec.textContent = count;
+    }
 
-	if (count === 10) {
-		fetchData();
-		count = 0;
-	}
-}, 1000);
+    // Emitir el contador al servidor para que lo retransmita a los jugadores
+    socket.emit('counter', count);
+
+    count--;
+
+    if (count < 0) {
+      clearInterval(interval);
+      fetchData();
+      count = 0;
+    }
+  }, 1000);
+});
+
+// Escuchar el evento 'clear-players-list' para limpiar la pantalla
+socket.on('clear-players-list', () => {
+  const container = document.getElementById('data-container');
+  container.innerHTML = ''; // Limpiar los datos anteriores
+});
+
+// Escuchar el nuevo evento para mostrar el nombre del jugador con item "?"
+socket.on('new-player', (player) => {
+  const container = document.getElementById('data-container');
+  const div = document.createElement('div');
+  div.className = 'item';
+  div.innerHTML = `
+    <p>Name: ${player.name}</p>
+    <p>Chose: ${player.item}</p>
+  `;
+  container.appendChild(div);
+});
 
 async function fetchData() {
-	renderLoadingState();
-	try {
-		const response = await fetch('http://localhost:5050/users');
-		if (!response.ok) {
-			throw new Error('Network response was not ok');
-		}
-		const data = await response.json();
-		renderData(data);
-	} catch (error) {
-		console.error(error);
-		renderErrorState();
-	}
-}
-
-async function clearPlayersList() {
-	try {
-		await fetch('http://localhost:5050/users', { method: 'DELETE' });
-		renderData({ players: [] }); // Limpia la pantalla
-		console.log('Player list cleared');
-	} catch (error) {
-		console.error('Failed to clear the list', error);
-	}
+  renderLoadingState();
+  try {
+    const response = await fetch('http://localhost:5050/users');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    renderData(data);
+  } catch (error) {
+    console.error(error);
+    renderErrorState();
+  }
 }
 
 function renderErrorState() {
-	const container = document.getElementById('data-container');
-	container.innerHTML = ''; // Limpiar datos anteriores
-	container.innerHTML = '<p>Failed to load data</p>';
-	console.log('Failed to load data');
+  const container = document.getElementById('data-container');
+  container.innerHTML = '<p>Failed to load data</p>';
+  console.log('Failed to load data');
 }
 
 function renderLoadingState() {
-	const container = document.getElementById('data-container');
-	container.innerHTML = ''; // Limpiar datos anteriores
-	container.innerHTML = '<p>Loading...</p>';
-	console.log('Loading...');
+  const container = document.getElementById('data-container');
+  container.innerHTML = '<p>Loading...</p>';
+  console.log('Loading...');
 }
 
 function renderData(data) {
-	const container = document.getElementById('data-container');
-	container.innerHTML = ''; // Limpiar datos anteriores
+  console.log(data);
 
-	if (data.players.length > 0) {
-		data.players.forEach((player) => {
-			const div = document.createElement('div');
-			div.className = 'item';
-
-			let itemImage;
-			switch (player.item) {
-				case 'Rock':
-					itemImage = 'https://www.pngall.com/wp-content/uploads/5/Stone-PNG-High-Quality-Image.png';
-					break;
-				case 'Paper':
-					itemImage =
-						'https://static.vecteezy.com/system/resources/previews/009/340/335/non_2x/white-crumpled-paper-balls-for-design-element-png.png';
-					break;
-				case 'Scissors':
-					itemImage = 'https://pngimg.com/d/scissors_PNG10.png';
-					break;
-			}
-
-			// Mostrar la imagen del ítem, el nombre y la opción elegida por el jugador
-			div.innerHTML = `
-        <p>Name: ${player.name}</p>
-        <p>Chose: ${player.item}</p>
-        <img src="${itemImage}" alt="${player.item}" style="width: 100px; height: auto;" />
-      `;
-
-			container.appendChild(div);
-		});
-	} else {
-		container.innerHTML = '<p>No players found</p>';
-	}
+  const container = document.getElementById('data-container');
+  container.innerHTML = ''; // Limpiar datos anteriores
+  data.players.forEach((player) => {
+    const section = document.createElement('section');
+    section.className = 'item';
+    section.innerHTML = `
+      <p>Name: ${player.name}</p>
+      <p>Chose: ${player.item}</p>
+    `;
+    container.appendChild(section);
+  });
 }
+
+
